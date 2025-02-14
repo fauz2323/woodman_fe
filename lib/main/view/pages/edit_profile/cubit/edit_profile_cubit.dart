1137b10dc5bd5fbf2616bc777abac8392a1a291a
Muhamad Faisal <61342260+Icankkkk@users.noreply.github.com>
2025-02-14 16:25:48 +0700

    Add edit profile feature with state management and routing

---
+++ lib/main/view/pages/edit_profile/cubit/edit_profile_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'edit_profile_state.dart';
+part 'edit_profile_cubit.freezed.dart';
+
+class EditProfileCubit extends Cubit<EditProfileState> {
+  EditProfileCubit() : super(const EditProfileState.initial());
+}
