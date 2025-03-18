f6bf786b775a60fdde760501f3c4e9464a279e2e
Zen <59063107+fauz2323@users.noreply.github.com>
2025-03-19 01:33:43 +0700

    Refactor PlaceOrderCubit and PlaceOrderState for improved state initialization

--- lib/main/view/pages/place_order/cubit/place_order_state.dart
+++ lib/main/view/pages/place_order/cubit/place_order_state.dart
@@ -8,6 +8,10 @@ class PlaceOrderState with _$PlaceOrderState {
   const factory PlaceOrderState.success() = _Success;
   // error
   const factory PlaceOrderState.error(String message) = _Error;
-  
-  const factory PlaceOrderState.initial() => const PlaceOrderState(bankTransfer: false, whatsappAdmin: true, cashOnDelivery: true);
+
+  const factory PlaceOrderState.initial(
+      {@Default(false) bool bankTransfer,
+      @Default(true) bool whatsappAdmin,
+      @Default(false) bool cashOnDelivery}) = _Initial;
+  // const factory PlaceOrderState.initial() => const PlaceOrderState(bankTransfer: false, whatsappAdmin: true, cashOnDelivery: true);
 }
